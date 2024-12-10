b93030933a17cf469bdaec1aa14ba5b604b37262
Meghareddy123 <meghama2004@gmail.com>
2024-12-10 13:47:33 -0300

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import google.generativeai as genai
 
 # Configure Google API key directly
-API_KEY = "** YOUR_APP_KEY **"
+API_KEY = os.getenv("GENAI_API_KEY")
 genai.configure(api_key=API_KEY)
 
 # Initialize session state
